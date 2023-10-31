cde26081ab63177a599efb78e80702d31e6aa0a3
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2023-10-31 11:54:49 -0300

    .

---
+++ MAIN/js/home.js
@@ -0,0 +1,38 @@
+function revelAviso1() {
+
+    var av1 = document.getElementById('aviso1')
+    
+    if (av1.style.display === 'none') {
+        av1.style.display = 'block'; // Mostra o conteúdo ao clicar
+
+    } else {
+        av1.style.display = 'none'; // Oculta o conteúdo se já estiver visível
+    }
+
+}
+
+function revelAviso2(){
+
+    var av2 = document.getElementById('aviso2')
+    
+    if (av2.style.display === 'none') {
+        av2.style.display = 'block'; // Mostra o conteúdo ao clicar
+
+    } else {
+        av2.style.display = 'none'; // Oculta o conteúdo se já estiver visível
+    }
+
+}
+
+function revelAviso3() {
+
+    var av3 = document.getElementById('aviso3')
+    
+    if (av3.style.display === 'none') {
+        av3.style.display = 'block'; // Mostra o conteúdo ao clicar
+        
+    } else {
+        av3.style.display = 'none'; // Oculta o conteúdo se já estiver visível
+    }
+
+}
