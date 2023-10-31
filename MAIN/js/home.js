674ba293805cc2fe48a317fd6e12fde7e14e244c
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2023-10-31 16:08:42 -0300

    .

--- MAIN/js/home.js
+++ MAIN/js/home.js
@@ -1,38 +1,66 @@
-function revelAviso1() {
+// CODIGO HOME
 
-    var av1 = document.getElementById('aviso1')
-    
-    if (av1.style.display === 'none') {
-        av1.style.display = 'block'; // Mostra o conteúdo ao clicar
+function showPage(id) {
 
-    } else {
-        av1.style.display = 'none'; // Oculta o conteúdo se já estiver visível
-    }
+    const subpag = document.querySelectorAll('.subpagina');
+    subpag.forEach(sp => {
+        sp.classList.remove('pagina');
+    });
 
-}
-
-function revelAviso2(){
-
-    var av2 = document.getElementById('aviso2')
-    
-    if (av2.style.display === 'none') {
-        av2.style.display = 'block'; // Mostra o conteúdo ao clicar
-
-    } else {
-        av2.style.display = 'none'; // Oculta o conteúdo se já estiver visível
-    }
+    const selectSubpag = document.getElementById(id);
+    selectSubpag.classList.add('pagina');
 
 }
 
-function revelAviso3() {
-
-    var av3 = document.getElementById('aviso3')
+function some(id) {
     
-    if (av3.style.display === 'none') {
-        av3.style.display = 'block'; // Mostra o conteúdo ao clicar
+    const pagagr = document.getElementById(id);
+    pagagr.classList.add('subpagina')
+
+}
+
+
+
+// CODIGO AVISO
+
+
+
+    // AVISOS
+    function revelAviso1() {
+
+        var av1 = document.getElementById('aviso1')
         
-    } else {
-        av3.style.display = 'none'; // Oculta o conteúdo se já estiver visível
+        if (av1.style.display === 'none') {
+            av1.style.display = 'block'; // Mostra o conteúdo ao clicar
+
+        } else {
+            av1.style.display = 'none'; // Oculta o conteúdo se já estiver visível
+        }
+
     }
 
-}
+    function revelAviso2(){
+
+        var av2 = document.getElementById('aviso2')
+        
+        if (av2.style.display === 'none') {
+            av2.style.display = 'block'; // Mostra o conteúdo ao clicar
+
+        } else {
+            av2.style.display = 'none'; // Oculta o conteúdo se já estiver visível
+        }
+
+    }
+
+    function revelAviso3() {
+
+        var av3 = document.getElementById('aviso3')
+        
+        if (av3.style.display === 'none') {
+            av3.style.display = 'block'; // Mostra o conteúdo ao clicar
+            
+        } else {
+            av3.style.display = 'none'; // Oculta o conteúdo se já estiver visível
+        }
+
+    }
